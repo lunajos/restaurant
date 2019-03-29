aa75ab2706c35110be5f97092a0c8cb3f0cfa0dd
lunajos <lunajose003@gmail.com>
2019-03-29 10:26:30 -0400

    Clean up JS

--- src/js/index.js
+++ src/js/index.js
@@ -6,29 +6,9 @@ let clientsXML = 'data/Clients_PhaseA.xml';
 let recipesXML = 'data/Recipes_PhaseB.xml'; // jshint ignore:line
 let ordersXML = 'data/Orders_PhaseC.xml'; // jshint ignore:line
 
-// fix paths and root
-let data = {
-	clients: {
-		path: 'data/Clients_PhaseA.xml',
-		root: 'client'
-	},
-	recipes: {
-		path: 'data/Recipes_PhaseB.xml',
-		root: 'recipe'
-	},
-	orders: {
-		path: 'data/Orders_PhaseC.xml',
-		root: 'clientOrder'
-	}
-};
-
-console.log(data);
-
 loadXML(clientsXML, 'client');
-//loadXML(recipesXML, 'recipe');
-//loadXML(ordersXML, 'clientOrder');
-
-
+loadXML(recipesXML, 'recipe');
+loadXML(ordersXML, 'clientOrder');
 
 function loadXML(file, root){
 	window.XMLHttpRequest	 // jshint ignore:line
@@ -48,18 +28,15 @@ function loadXML(file, root){
 }
 
 function showXML(xml, root){
-		let x, i, box, isFirstEl, info;					
+		let x, i, box, info;					
 		xmlDoc=xml.responseXML;		
-		isFirstEl= true;
 		content = xmlDoc.getElementsByTagName(root);
 	
 		for (i = 0; i < content.length; i++) {
 			box = document.getElementsByClassName(root); // jshint ignore: line
 			box[0].innerHTML += '<div class="root">';
-			console.log(box[i]);
 
 			info = document.getElementsByClassName('root'); // jshint ignore: line
-			//console.log(box[0].innerHTML += '<div class="' + root + '">' );
 
 			for (x = 0; x < content[i].childNodes.length; x++) {
 				 if (content[i].childNodes[x].textContent.length) { // xml content to DOM
@@ -70,5 +47,3 @@ function showXML(xml, root){
 			info[i].innerHTML += '</div>';
 		} // end
 }
-
-
